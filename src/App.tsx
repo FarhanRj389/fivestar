eacbc67163e93deaeaed9e3cead8a60b169c5c59
Farhan <farhanrjcw389@gmail.com>
2025-08-05 14:40:52 +0500

    updaer time increase

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 1200);
+    const timer = setTimeout(() => setLoading(false), 5000);
     return () => clearTimeout(timer);
   }, []);
 
